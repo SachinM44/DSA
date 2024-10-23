3e8c26b49a626e2ace42b20ebb74fa40e6eaf617
xdevgit <xdev065@gmail.com>
2024-10-23 20:37:54 +0530

    rev the array

---
+++ reverseTheArray.cpp
@@ -0,0 +1,16 @@
+class salution{
+    public: 
+    void revesersethearra(vector <int> &arr){
+        int n=size.arr();
+        int start = 0;
+        int end = n-1;
+        while(start <= end ){
+            for(int i=0;i<n; i++){
+                swap(arrr[start], arr[end]);
+                start++;
+                end--;
+            }
+        }
+    }
+    return; 
+}
