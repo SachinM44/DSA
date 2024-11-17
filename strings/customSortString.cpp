64d9f8677bc9e5584b1775b2d16f990c254928fc
xdevgit <xdev065@gmail.com>
2024-11-17 23:09:30 +0530

    791. Custom Sort String

--- strings/customSortString.cpp
+++ strings/customSortString.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
 static string str;
-static bool compare(char char1, char char2){
+static bool sortNcompare(char char1, char char2){
     return(str.find(char1)<str.find(char2));
 }
     string customSortString(string order, string s) {
         str = order;
-        sort(s.begin(),s.end(),compare);
+        sort(s.begin(),s.end(),sortNcompare);
         return s;
     }
 };
