3b749e31fb39f0012e60bc4e5442a6a8e526af92
SachinM44 <sachin69778@gmail.com>
2025-02-24 12:38:00 +0530

    2sum in js

--- array/2sum.cpp
+++ array/2sum.cpp
@@ -1,14 +1,27 @@
-class Solution {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            int n=nums.size();
-            for(int i=0;i<n;i++){
-                for( int j=i+1; j<n;j++ ){
-                    if(nums[i]+nums[j]==target){
-                        return {i,j};
-                    }
-                }
+// class Solution {
+//     public:
+//         vector<int> twoSum(vector<int>& nums, int target) {
+//             int n=nums.size();
+//             for(int i=0;i<n;i++){
+//                 for( int j=i+1; j<n;j++ ){
+//                     if(nums[i]+nums[j]==target){
+//                         return {i,j};
+//                     }
+//                 }
+//             }
+//             return {};
+//         }
+//     };
+//////////////////////////////////
+///in js
+function twoSum(nums, target){
+    let n=nums.lenght();
+    for(let i=0; i<n ; i++){
+        for(let j=i+1; j<n; j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j]// in the cpp u use vector , so you have to use {} insted of []
             }
-            return {};
         }
-    };
+        return [];
+    }
+} ;
