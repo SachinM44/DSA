cb8d5e42d17d5adad4b9c58f9c5c597b6ba7945d
SachinM44 <sachin69778@gmail.com>
2026-01-15 19:36:57 +0530

    feat: added all

---
+++ OOPS/class.cpp
@@ -0,0 +1,33 @@
+#include <iostream>;
+#include <string>;
+using namespace std;
+
+class teacher
+{
+    ///access modifier// and by defaul is privet
+    public:
+    // properties/// atributes
+    string name;
+    string department;
+    int age;
+    float salary;
+    // methods
+///////// this is also called member fun 
+    void changeName(string DepTvalue)
+    {
+        department = DepTvalue;
+    }
+};
+
+/// this will teh main fn
+int main()
+{
+
+    /// now the objects ->
+  teacher t1;
+  t1.name='mallya';
+
+  teacher t2;
+  /// this will be the n number of the objects
+  return 0;
+}
