a1e43387927d77b622a5ab754beffff711c54804
SachinM44 <sachin69778@gmail.com>
2026-01-16 12:16:04 +0530

    feat: added somthing

--- seachAndsorting/binarySearch.cpp
+++ seachAndsorting/binarySearch.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int binarySearch(vector<int> &arr, int target)
-{
-    int left = 0, right = arr.size() - 1;
-    int mid = left + (right - left) / 2;
-
-    while (left <= right)
-    {
-        if (arr[mid] == target)
-            return mid;
-    }
-    if (arr[mid] < target)
-    {
-        return left = mid + 1;
-    }
-    else
-    {
-        return right = mid - 1;
-    }
-
-    return -1;
+int binarySearch(vector <int>& arr, int target){
+ int left=0, right=arr.size();
+ int mid=left+(right-left)/2;
+ while(left <= right ){
+  if(arr[mid]==target) return mid;
+  else if(arr[mid] < target){
+         left=arr[mid]+1;
+  }else{
+    right=arr[mid]-1;
+  }
+ }
 }
 
 //tc 0(log n), sc 0(1)
