9757f9a0f8a9e00194f415e663bbd48894304082
SachinM44 <sachin69778@gmail.com>
2026-01-29 14:29:01 +0530

    wprngin on somthing

--- stacksAndQues/stackWithArray.cpp
+++ stacksAndQues/stackWithArray.cpp
@@ -3,48 +3,62 @@ using namespace std;
 
 class Stack
 {
-    int arr[5];
-    int topIndex;
 
+    int arr[10];
+    int topIndex; //  how to define the top index
 public:
-    /// @brief this is the constructor 
     Stack()
     {
-        topIndex = -1;
-    };
+        topIndex = -1; // here construct it again with - value , so it wont cantain the garbage value
+    }
 
     void push(int value)
     {
-        if (topIndex == 4)
+        if (topIndex == 9)
         {
             cout << "the stack is full" << endl;
             return;
-        };
-
+        }
         arr[++topIndex] = value;
     }
-    void pop(){
-        if(topIndex== -1){
-            cout << "the stack is already emty" << endl;
+
+    void pop()
+    {
+        if (topIndex == -1)
+        { // mesna the stack is alredy empty
+            cout << "stack is already empty" << endl;
             return;
         }
         topIndex--;
     }
 
-    int top(){
-        if(topIndex ==-1){
-            throw runtime_error("stack is empty");
+    int top()
+    {
+        if (topIndex == -1)
+        { // mesna the stack is alredy empty
+            throw runtime_error("stack is already empty");
         }
+
         return arr[topIndex];
     }
 
-    bool isEmpt(){
-        return topIndex==-1;
+    bool isEmpty()
+    {
+        return topIndex == -1;
     }
-
 };
 
 int main()
 {
+    Stack s;
+    s.push(33);
+    s.push(3344);
+
+    while (!s.isEmpty())
+    {
+        cout << s.top() << " " << endl;
+        s.pop();
+    }
+
     return 0;
 }
