18983466fab4a238a4efa5226087670541285b71
SachinM44 <sachin69778@gmail.com>
2026-01-31 18:29:46 +0530

    feat/woring on the doubly ll

--- LinkList/doublyLL.cpp
+++ LinkList/doublyLL.cpp
@@ -11,9 +11,10 @@ public:
     Node *prev;
     Node *next;
 
-    Node(int value){
-        data=value;
-        prev=next=NULL;
+    Node(int value)
+    {
+        data = value;
+        prev = next = NULL;
     };
 }; // so wee need another class for doubly ll
 
@@ -29,21 +30,53 @@ public:
         head = tail = NULL;
     }
 
+    void push_front(int value)
+    {
+        Node *newNode = new Node(value);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode; // at final make that new node to head
+        }
+    }
 
-    void push_front(int value){
-   Node* newNode=new Node(value);
-      if(head==NULL){
-        head=tail=newNode;
-      }else{
-        newNode->next=head;
-        head->prev=newNode;
-        head=newNode;//at final make that new node to head 
-      }
+    void push_back(int value)
+    {
+        Node *newNode = new Node(value);
+        if (head == NULL)
+        {
+            head=tail=newNode;
+        }else{
+            head->next=newNode;
+            newNode->prev=head;
+
+        }
+    }
+
+    void print_all()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " <=> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
     }
 };
 
-
 int main()
 {
+
+    doublyLl dd;
+    dd.push_front(4);
+    dd.push_front(37);
+    dd.push_back(44);
+    dd.print_all();
     return 0;
 }
